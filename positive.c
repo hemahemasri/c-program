8f3196c8693fc15fd8bc46c1cc45551c95d3fe94
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2017-08-05 16:04:44 +0530

    d
    dd

---
+++ positive.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+double number;
+ int i;
+ printf("enter the number:");
+ if(i<0)
+ {
+ printf("the entered number is negative:");
+ }else
+ {
+ printf("entered number is positive:");
+ }
+ }
